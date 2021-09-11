7c7436dc9bdd6fba763671088df3eb72cf1c2ddf
kel-man <kyyafuso@email.com>
2021-09-10 21:58:39 -0700

    adds specs and validations for pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,3 @@
 class Pet < ApplicationRecord
+  validates :name, :species, :sex, :fixed, :available, :breed, :state, presence: true
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(
+      name: 'name',
+      species: 'species',
+      sex: 'sex',
+      breed: 'breed',
+      vaccinations: false,
+      medical: ['array', 'of', 'strings'],
+      behavior: 'text',
+      fixed: true,
+      available: true,
+      age: 10,
+      description: 'some more text',
+      city: 'a city',
+      state: 'Alaska',
+      housetrained: true,
+      declawed: true,
+      lived_with_children: false,
+      lived_with_animals: false,
+      img_caption: 'this is a picture of someone elses dog',
+      user_id: 1
+    )
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without species' do
+    subject.species = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without sex' do
+    subject.sex = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without fixed' do
+    subject.fixed = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without available' do
+    subject.available = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without breed' do
+    subject.breed = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without state' do
+    subject.state = nil
+    expect(subject).to_not be_valid
+  end
 end
