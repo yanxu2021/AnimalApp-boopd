16117460830233731b583f1495fd86d43f996a71
yanxu2021 <yan.xu2021@outlook.com>
2021-09-14 14:16:13 -0700

    add footer and aboutus

--- app/javascript/components/components/AppRouter.js
+++ app/javascript/components/components/AppRouter.js
@@ -5,10 +5,12 @@ import {
     Switch
 } from 'react-router-dom'
 import Header from './Header'
+import Footer from './Footer'
 import SignUp from '../pages/SignUp'
 import SignIn from '../pages/SignIn'
 import PetNew from '../pages/PetNew'
 import NotFound from '../pages/NotFound'
+import AboutUs from '../pages/AboutUs'
 
 class AppRouter extends React.Component {
     render() {
@@ -20,8 +22,10 @@ class AppRouter extends React.Component {
                   <Route path='/signup' component={SignUp}/>
                   <Route path='/signin' component={SignIn}/>
                   {this.props.loggedIn && <Route path='/petnew' component={PetNew}/> }
+                  <Route path='/aboutus' component={AboutUs}/>
                   <Route component={NotFound}/>
                 </Switch>
+                <Footer/>
             </Router>
         )
     }
