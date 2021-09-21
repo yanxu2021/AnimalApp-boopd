fbac2fa1e4e11f3386b36cdb31cf850d0932d6e3
kel-man <kyyafuso@email.com>
2021-09-21 10:50:12 -0700

    adds labels to top of index and new page, adds information to about-us page, removes console log exposing auth context

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,7 +4,6 @@ import AppRouter from "./components/AppRouter";
 class App extends React.Component {
 
   render () {
-    console.log(this.props)
     return (
       <React.Fragment>
         <AppRouter loggedIn={this.props.logged_in} currentUser={this.props.current_user}/>
--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -19,25 +19,25 @@ class AboutUs extends React.Component {
           <div className="blurb">
             <img src={kelen} alt="about"/>
             <h5>Kelen</h5>
-            <p>some stuff about person</p>
+            <p>Tech Lead</p>
             <Button className="button" target="_blank" href="https://github.com/kel-man">GitHub</Button>
           </div>
           <div className="blurb">
             <img src={yan} alt="about"/>
             <h5>Yan</h5>
-            <p>some stuff about person</p>
+            <p>Project Manager</p>
             <Button className="button" target="_blank" href="https://github.com/yanxu2021">GitHub</Button>
           </div>
           <div className="blurb">
             <img src={amanda} alt="about"/>
             <h5>Amanda</h5>
-            <p>some stuff about person</p>
+            <p>Design Lead</p>
             <Button className="button" target="_blank" href="https://github.com/AFerg15">GitHub</Button>
           </div>
           <div className="blurb">
             <img src={chris} alt="about"/>
             <h5>Chris</h5>
-            <p>some stuff about person</p>
+            <p>Product Manager</p>
             <Button className="button" target="_blank" href="https://github.com/Chrisluna10">GitHub</Button>
           </div>
         </div>
